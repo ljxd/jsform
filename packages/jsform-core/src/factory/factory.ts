2f0c9a582930620a151f51a4cce672bbd39de0ab
nick121212 <xiao.feng@weimob.com>
2019-04-22 21:26:34 +0800

    优化注释

--- packages/jsform-core/src/factory/factory.ts
+++ packages/jsform-core/src/factory/factory.ts
@@ -27,7 +27,10 @@ export const schemaFieldFactory = new BaseFactory<FxJsonSchema>();
  */
 export const schemaKeyWordFactory = new BaseFactory<($id: string, schema: JSONSchema6) => JSONSchema6>();
 /**
- * 
+ * 存放类型的工厂
  */
 export const schemaTypeFactory = new BaseFactory<($id: string, schema: JSONSchema6) => JSONSchema6>();
+/**
+ * 存放jsonkey与schemaPath的对应关系
+ */
 export const schemaKeysFactory = new BaseFactory<string>();
