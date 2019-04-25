91bf73b553cc60a1cf09fd5c4c8bde3e613827e9
nick121212 <xiao.feng@weimob.com>
2019-04-25 19:10:40 +0800

    data设计API

--- packages/jsform-data/src/libs/base.ts
+++ packages/jsform-data/src/libs/base.ts
@@ -3,65 +3,69 @@ import invariant from "invariant";
 import {JSFormData, JSFormDataModel} from "../models/data";
 import {TreeMap} from "../../../jsform-core/src/libs/tree";
 
-export class JSFormDataBase<GD, GM> implements JSFormData<GD, GM> {
-	/**
-	 * Creates an instance of JSFormDataBase.
-	 * @param {JSFormData<GD, GM>} actualJSFormData 传入实际的
-	 * @memberof JSFormDataBase
-	 */
-	constructor(private actualJSFormData: JSFormData<GD, GM>) {}
-	/**
-	 * 判定是否传入了真实的JSFormData
-	 * @private
-	 * @returns {boolean}
-	 * @memberof JSFormDataBase
-	 */
-	private checkJSFormData(): JSFormData<GD, GM> {
-		invariant(!!this.actualJSFormData, "JSFormData没有初始化!");
 
-		return this.actualJSFormData;
-	}
-	public getFormStore(): {[key: string]: JSFormDataModel<GD, GM>} {
-		return this.checkJSFormData().getFormStore();
-	}
-	public createForm(_formKey: string, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, TreeMap<GM>> {
-		return {
-			data: defaultData || (null) as any,
-			meta: defaultMeta || new TreeMap<GM>("root", {})
-		};
-	}
-	public removeForm(_formKey: string) {}
-	public resetForm(formKey: string, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().resetForm(formKey, defaultData, defaultMeta);
-	}
-	public updateItemData<D>(formKey: string, keys: Array<string | number>, data: D): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().updateItemData<D>(formKey, keys, data);
-	}
-	public updateItemMeta<M>(formKey: string, keys: Array<string | number>, meta: TreeMap<M>, isMerge = true): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().updateItemMeta<M>(formKey, keys, meta, isMerge);
-	}
-	public removeItem(formKey: string, keys: Array<string | number>, keepData: boolean, keepMeta: boolean): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().removeItem(formKey, keys, keepData, keepMeta);
-	}
-	public addArrayItem<D, M>(formKey: string, keys: Array<string | number>, data?: D, meta?: TreeMap<M>): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().addArrayItem<D, M>(formKey, keys, data, meta);
-	}
-	public removeArrayItem(formKey: string, keys: Array<string | number>, index: number): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().removeArrayItem(formKey, keys, index);
-	}
-	public moveArrayItem(formKey: string, keys: Array<string | number>, currentIndex: number, toIndex: number): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().moveArrayItem(formKey, keys, currentIndex, toIndex);
-	}
-	public getFormData(formKey: string): JSFormDataModel<GD, GM> {
-		return this.checkJSFormData().getFormData(formKey);
-	}
-	public getItemData<D>(formKey: string, keys: Array<string | number>): D {
-		return this.checkJSFormData().getItemData<D>(formKey, keys);
-	}
-	public getItemMeta<M>(formKey: string, keys: Array<string | number>): TreeMap<M> {
-		return this.checkJSFormData().getItemMeta<M>(formKey, keys);
-	}
-	public getItemDataAndMeta<D, M>(formKey: string, keys: Array<string | number>): JSFormData<D, M> {
-		return this.checkJSFormData().getItemDataAndMeta<D, M>(formKey, keys);
-	}
-}
+
+// const checkJSFormData
+
+// export class JSFormDataBase<GD, GM> implements JSFormData<GD, GM> {
+// 	/**
+// 	 * Creates an instance of JSFormDataBase.
+// 	 * @param {JSFormData<GD, GM>} actualJSFormData 传入实际的
+// 	 * @memberof JSFormDataBase
+// 	 */
+// 	constructor(private actualJSFormData: JSFormData<GD, GM>) {}
+// 	/**
+// 	 * 判定是否传入了真实的JSFormData
+// 	 * @private
+// 	 * @returns {boolean}
+// 	 * @memberof JSFormDataBase
+// 	 */
+// 	private checkJSFormData(): JSFormData<GD, GM> {
+// 		invariant(!!this.actualJSFormData, "JSFormData没有初始化!");
+
+// 		return this.actualJSFormData;
+// 	}
+// 	public getFormStore(): {[key: string]: JSFormDataModel<GD, GM>} {
+// 		return this.checkJSFormData().getFormStore();
+// 	}
+// 	public createForm(_formKey: string, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, TreeMap<GM>> {
+// 		return {
+// 			data: defaultData || (null) as any,
+// 			meta: defaultMeta || new TreeMap<GM>("root", {})
+// 		};
+// 	}
+// 	public removeForm(_formKey: string) {}
+// 	public resetForm(formKey: string, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().resetForm(formKey, defaultData, defaultMeta);
+// 	}
+// 	public updateItemData<D>(formKey: string, keys: Array<string | number>, data: D): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().updateItemData<D>(formKey, keys, data);
+// 	}
+// 	public updateItemMeta<M>(formKey: string, keys: Array<string | number>, meta: TreeMap<M>, isMerge = true): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().updateItemMeta<M>(formKey, keys, meta, isMerge);
+// 	}
+// 	public removeItem(formKey: string, keys: Array<string | number>, keepData: boolean, keepMeta: boolean): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().removeItem(formKey, keys, keepData, keepMeta);
+// 	}
+// 	public addArrayItem<D, M>(formKey: string, keys: Array<string | number>, data?: D, meta?: TreeMap<M>): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().addArrayItem<D, M>(formKey, keys, data, meta);
+// 	}
+// 	public removeArrayItem(formKey: string, keys: Array<string | number>, index: number): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().removeArrayItem(formKey, keys, index);
+// 	}
+// 	public moveArrayItem(formKey: string, keys: Array<string | number>, currentIndex: number, toIndex: number): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().moveArrayItem(formKey, keys, currentIndex, toIndex);
+// 	}
+// 	public getFormData(formKey: string): JSFormDataModel<GD, GM> {
+// 		return this.checkJSFormData().getFormData(formKey);
+// 	}
+// 	public getItemData<D>(formKey: string, keys: Array<string | number>): D {
+// 		return this.checkJSFormData().getItemData<D>(formKey, keys);
+// 	}
+// 	public getItemMeta<M>(formKey: string, keys: Array<string | number>): TreeMap<M> {
+// 		return this.checkJSFormData().getItemMeta<M>(formKey, keys);
+// 	}
+// 	public getItemDataAndMeta<D, M>(formKey: string, keys: Array<string | number>): JSFormData<D, M> {
+// 		return this.checkJSFormData().getItemDataAndMeta<D, M>(formKey, keys);
+// 	}
+// }
--- packages/jsform-data/src/models/data.ts
+++ packages/jsform-data/src/models/data.ts
@@ -22,13 +22,12 @@ export interface JSFormData<GD, GM> {
 	/**
 	 * 创建Form，初始化Form数据
 	 * 返回创建Form的数据结构
-	 * @param    {string}    formKey                    表单唯一标志，如果重复，则使用同一份数据
 	 * @param 	 {GD}        [defaultData] 				默认Data
 	 * @param 	 {GM}        [defaultMeta]				默认Meta
 	 * @returns  {JSFormDataModel<D, M>}                表单的数据
 	 * @memberof JSFormData
 	 */
-	createForm(formKey: string, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, GM>;
+	createForm(defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, GM>;
 	/**
 	 * 删除Form，以及所有的数据
 	 * @param    {string}    formKey                    表单唯一标志，如果重复，则使用同一份数据
@@ -38,84 +37,84 @@ export interface JSFormData<GD, GM> {
 	/**
 	 * 重置表单数据
 	 * 返回更新后的表单数据结构
-	 * @param    {string}    formKey                    表单唯一标志，如果重复，则使用同一份数据
-	 * @param 	 {GD}        [defaultData] 				默认Data
-	 * @param 	 {GM}        [defaultMeta]				默认Meta
-	 * @returns {JSFormDataModel<GD, GM>}				表单的数据
+	 * @param    {JSFormDataModel<GD, GM>}    formData                  表单数据
+	 * @param 	 {GD}                         [defaultData] 			默认Data
+	 * @param 	 {GM}                         [defaultMeta]				默认Meta
+	 * @returns {JSFormDataModel<GD, GM>}								表单的数据
 	 * @memberof JSFormData
 	 */
-	resetForm(formKey: string, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, GM>;
+	resetForm(formData: JSFormDataModel<GD, GM>, defaultData?: GD, defaultMeta?: TreeMap<GM>): JSFormDataModel<GD, GM>;
 	/**
 	 * 更新Form中单个元素的数据
 	 * 返回更新后的表单数据结构
 	 * @template D data的类型
-	 * @param    {string}    				formKey     表单唯一标志，如果重复，则使用同一份数据
+	 * @param   {JSFormDataModel<GD, GM>}   formData    表单数据
 	 * @param   {(Array<string | number>)}  keys        元素的json路径
 	 * @param   {D} 						data        需要更新的数据
 	 * @returns {JSFormDataModel<D, M>}                 表单的数据
 	 * @memberof JSFormData
 	 */
-	updateItemData<D>(formKey: string, keys: Array<string | number>, data: D): JSFormDataModel<GD, GM>;
+	updateItemData<D>(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>, data: D): JSFormDataModel<GD, GM>;
 	/**
 	 * 更新Form中单个元素的Meta
 	 * 返回更新后的表单数据结构
 	 * @template M meta的类型
-	 * @param    {string}    				formKey     表单唯一标志，如果重复，则使用同一份数据
+	 * @param   {JSFormDataModel<GD, GM>}   formData    表单数据
 	 * @param   {(Array<string | number>)}  keys        元素的json路径
 	 * @param   {D} 						meta        需要更新的meta
 	 * @param   {boolean}					isMerge     需要合并Meta
 	 * @returns {JSFormDataModel<D, M>}                 表单的数据
 	 * @memberof JSFormData
 	 */
-	updateItemMeta<M>(formKey: string, keys: Array<string | number>, meta: TreeMap<M>, isMerge: boolean): JSFormDataModel<GD, GM>;
+	updateItemMeta<M>(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>, meta: TreeMap<M>, isMerge: boolean): JSFormDataModel<GD, GM>;
 	/**
 	 * 删除一个元素
 	 * 删除元素的Data和Meta
 	 * 返回更新后的表单数据结构
-	 * @param    {string}    				formKey     表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys        元素的json路径
-	 * @param   {boolean}  					keepData    是否保留Data
-	 * @param   {boolean}  					keepMeta    是否保留Meta
-	 * @returns {JSFormDataModel<D, M>}                 返回更新后的表单数据结构
+	 * @param    {JSFormDataModel<GD, GM>}      formData    表单数据
+	 * @param    {(Array<string | number>)}     keys        元素的json路径
+	 * @param    {boolean}  					keepData    是否保留Data
+	 * @param    {boolean}  					keepMeta    是否保留Meta
+	 * @returns  {JSFormDataModel<D, M>}                    返回更新后的表单数据结构
 	 * @memberof JSFormData
 	 */
-	removeItem(formKey: string, keys: Array<string | number>, keepData: boolean, keepMeta: boolean): JSFormDataModel<GD, GM>;
+	removeItem(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>, keepData: boolean, keepMeta: boolean): JSFormDataModel<GD, GM>;
 	/**
 	 * 数组元素中添加一个子元素
 	 * 返回更新后的表单数据结构
 	 * @template D
 	 * @template M
-	 * @param    {string}    				formKey     表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys        父元素的json路径
-	 * @param   {D}                         [data]      子元素的Data
-	 * @param   {M}                         [meta]      子元素的Meta
-	 * @returns {JSFormDataModel<GD, GM>}				返回更新后的表单数据结构
+	 * @param    {JSFormDataModel<GD, GM>}   formData    表单数据
+	 * @param    {(Array<string | number>)}  keys        父元素的json路径
+	 * @param    {D}                         [data]      子元素的Data
+	 * @param    {M}                         [meta]      子元素的Meta
+	 * @returns  {JSFormDataModel<GD, GM>}				 返回更新后的表单数据结构
 	 * @memberof JSFormData
 	 */
-	addArrayItem<D, M>(formKey: string, keys: Array<string | number>, data?: D, meta?: TreeMap<M>): JSFormDataModel<GD, GM>;
+	addArrayItem<D, M>(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>, data?: D, meta?: TreeMap<M>): JSFormDataModel<GD, GM>;
 	/**
 	 * 数组元素中添加一个子元素
 	 * 返回更新后的表单数据结构
 	 * @template D
 	 * @template M
-	 * @param    {string}    			    formKey     表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys        父元素的json路径
-	 * @param   {number}                    index       子元素的Meta
-	 * @returns {JSFormDataModel<GD, GM>}				返回更新后的表单数据结构
+	 * @param    {JSFormDataModel<GD, GM>}   formData    表单数据
+	 * @param    {(Array<string | number>)}  keys        父元素的json路径
+	 * @param    {number}                    index       子元素的Meta
+	 * @returns  {JSFormDataModel<GD, GM>}				 返回更新后的表单数据结构
 	 * @memberof JSFormData
 	 */
-	removeArrayItem(formKey: string, keys: Array<string | number>, index: number): JSFormDataModel<GD, GM>;
+	removeArrayItem(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>, index: number): JSFormDataModel<GD, GM>;
 	/**
 	 * 数组元素移动位置
 	 * 返回更新后的表单数据结构
-	 * @param    {string}    				formKey       表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys          父元素的json路径
-	 * @param   {number} 					currentIndex  当前元素的索引
-	 * @param   {number} 					toIndex       需要移动到的索引
-	 * @returns {JSFormDataModel<GD, GM>}				  返回更新后的表单数据结构
+	 * @param    {JSFormDataModel<GD, GM>}   formData     表单数据
+	 * @param    {(Array<string | number>)}  keys         父元素的json路径
+	 * @param    {number} 					currentIndex  当前元素的索引
+	 * @param    {number} 					toIndex       需要移动到的索引
+	 * @returns  {JSFormDataModel<GD, GM>}				  返回更新后的表单数据结构
 	 * @memberof JSFormData
 	 */
-	moveArrayItem(formKey: string, keys: Array<string | number>, currentIndex: number, toIndex: number): JSFormDataModel<GD, GM>;
+	moveArrayItem(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>, currentIndex: number, toIndex: number): JSFormDataModel<GD, GM>;
 	/**
 	 * 获取单个表单的数据
 	 * 包括Data和Meta
@@ -128,30 +127,30 @@ export interface JSFormData<GD, GM> {
 	 * 获取单个元素的Data
 	 * 返回数据类型
 	 * @template D
-	 * @param    {string}    				formKey       表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys          父元素的json路径
-	 * @returns {D}
+	 * @param    {JSFormDataModel<GD, GM>}   formData    表单数据
+	 * @param    {(Array<string | number>)}  keys        父元素的json路径
+	 * @returns  {D}
 	 * @memberof JSFormData
 	 */
-	getItemData<D>(formKey: string, keys: Array<string | number>): D;
+	getItemData<D>(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>): D;
 	/**
 	 * 获取单个元素的Meta
 	 * 返回数据类型
 	 * @template M
-	 * @param    {string}                   formKey       表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys          父元素的json路径
-	 * @returns {M}
+	 * @param    {JSFormDataModel<GD, GM>}   formData    表单数据
+	 * @param    {(Array<string | number>)}  keys        父元素的json路径
+	 * @returns  {M}
 	 * @memberof JSFormData
 	 */
-	getItemMeta<M>(formKey: string, keys: Array<string | number>): TreeMap<M>;
+	getItemMeta<M>(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>): TreeMap<M>;
 	/**
 	 * 获取单个元素的Data和Meta
 	 * @template D
 	 * @template M
-	 * @param    {string}                   formKey       表单唯一标志，如果重复，则使用同一份数据
-	 * @param   {(Array<string | number>)}  keys          父元素的json路径
-	 * @returns {JSFormData<D, M>}
+	 * @param    {JSFormDataModel<GD, GM>}   formData    表单数据
+	 * @param    {(Array<string | number>)}  keys        父元素的json路径
+	 * @returns  {JSFormData<D, M>}
 	 * @memberof JSFormData
 	 */
-	getItemDataAndMeta<D, M>(formKey: string, keys: Array<string | number>): JSFormData<D, M>;
+	getItemDataAndMeta<D, M>(formData: JSFormDataModel<GD, GM>, keys: Array<string | number>): JSFormData<D, M>;
 }
